f8d899c0aac88fa1a6fa0e873a3ee3eab44bd04b
yifan-sco-20 <88814403+yifan-sco-20@users.noreply.github.com>
2021-10-12 21:26:19 +0930

    Connected Back-end to Auth0 API

---
+++ TalkingToTheSpaceAngularNTierApp/TalkingToTheSpaceAngularNTierApp/Controllers/ApiController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            return Ok(new
+            {
+                Message = "Hello from a public endpoint! You don't need to be authenticated to see this."
+            });
+        }
+
+        [HttpGet("private")]
+        [Authorize]
+        public IActionResult Private()
+        {
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated to see this."
+            });
+        }
+
+        [HttpGet("private-scoped")]
+        [Authorize("read:messages")]
+        public IActionResult Scoped()
+        {
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+            });
+        }
+
+        // This is a helper action. It allows you to easily view all the claims of the token.
+        [HttpGet("claims")]
+        public IActionResult Claims()
+        {
+            return Ok(User.Claims.Select(c =>
+                new
+                {
+                    c.Type,
+                    c.Value
+                }));
+        }
+    }
+}
