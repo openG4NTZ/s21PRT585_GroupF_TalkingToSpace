4c5dbb293d7364d118f931d0b5f2c295c087e29a
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-10-13 12:17:27 +0930

    update

--- TalkingToSpace_Angular/src/app/post/post.component.ts
+++ TalkingToSpace_Angular/src/app/post/post.component.ts
@@ -144,14 +144,8 @@ export class PostComponent implements OnInit {
     let result = new MessageResult();
 
     this.currentMessage.message_id=message_id;
-<<<<<<< HEAD
-<<<<<<< HEAD
     this.currentMessage.message_content="";
     this.currentMessage.message_status='private';
-=======
->>>>>>> parent of c9711fe (Merge branch 'Development' of https://github.com/openG4NTZ/s21PRT585_GroupF_TalkingToSpace into Development)
-=======
->>>>>>> parent of c9711fe (Merge branch 'Development' of https://github.com/openG4NTZ/s21PRT585_GroupF_TalkingToSpace into Development)
     this.currentMessage.user_id=2;
     await this.MessageService
       .deleteMessage(this.currentMessage)
