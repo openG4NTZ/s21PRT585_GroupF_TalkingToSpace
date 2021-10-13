9e924431a063f4570add3b8264f49e96522c8e2c
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-10-13 15:59:58 +0930

    add tournament V9.1

---
+++ TalkingToSpace_Angular/src/app/student.ts
@@ -0,0 +1,14 @@
+export class Student{
+  _id:number
+    balance:number;
+          grade:number;
+          age:number;
+          eyeColor: string;
+          name: string;
+          gender: boolean;
+          company: string;
+          email: string;
+          phone: string;
+          addres: string;
+
+  }
--- TalkingToSpace_Angular/src/app/tournament/tournament.component.ts
+++ TalkingToSpace_Angular/src/app/tournament/tournament.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-tournament',
@@ -8,10 +9,49 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class TournamentComponent implements OnInit {
 
+  studentdsJson: Array<Student> = new Array<Student>();
+  copyStudentdsJson: Array<Student> = new Array<Student>();
+  showBool: boolean;
+  str: string = "";
+  aboveFifty = (x: Student): boolean => { return x.grade >= 50 };
+  underFifty = (x: Student): boolean => { return x.grade < 50 };
+  grade100 = (x: Student): boolean => { return x.grade == 100 };
+  searchStr = (x: Student): boolean => {
+     return x.name.toLowerCase().indexOf(this.strSearch, 0) != -1
+
+     };
+
+  strSearch: string;
 
   constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
   }
+
+  initialize() {
+    for (let i: number = 0; i < this.studentdsJson.length; i++) {
+      this.copyStudentdsJson[i] = new Student();
+      Object.assign(this.copyStudentdsJson[i], this.studentdsJson[i]);
+    }
+  }
+
+  showList() {
+    this.initialize();
+    this.showBool = true;
+  }
+
+  searchFunc(str:string) {
+    this.initialize();
+    this.strSearch = str;
+    this.filterResults(this.searchStr);
+
+  }
+
+
+  filterResults(funcParam: any) {
+    this.showBool = true;
+    this.copyStudentdsJson = this.studentdsJson.filter(funcParam);
+  }
+
 
 }
