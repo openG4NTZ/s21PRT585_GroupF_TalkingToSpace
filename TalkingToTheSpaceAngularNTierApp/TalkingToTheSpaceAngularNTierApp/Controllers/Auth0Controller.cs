cc04fd52975966f46ad43e7763d00d5f297adeff
openG4NTZ <62043773+openG4NTZ@users.noreply.github.com>
2021-10-13 11:26:18 +0930

    merge

---
+++ TalkingToTheSpaceAngularNTierApp/TalkingToTheSpaceAngularNTierApp/Controllers/Auth0Controller.cs
@@ -0,0 +1,30 @@
+﻿using LOGIC.Services.Implementation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class Auth0Controller
+    {
+
+        [HttpPost]
+        public async void Auth0Post()
+        {
+            var client = new RestClient("https://dev-om53k5ag.us.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"client_id\":\"Eq54gdPB6yDfhHTB8OM9BqMR4c8YPbNu\",\"client_secret\":\"-xGe0vdswBk6qGKf2zSLTMM6bNQkUxc8PBxqhDprl-eeovVXlLFXYaWFmnJZX8DK\",\"audience\":\"https://talking-to-the-space/user/api\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content.ToString());
+ 
+        }
+
+    }
+}
